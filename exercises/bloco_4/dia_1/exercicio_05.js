121074123fcf8a869a6566ebfe9bcd0093a9c0ba
Cleberton Francisco <clebertonfgc@gmail.com>
2020-10-23 20:56:42 -0300

    Criando exercicio 05 angulos de um triangulo

---
+++ exercises/bloco_4/dia_1/exercicio_05.js
@@ -0,0 +1,16 @@
+// Exercicio 05 angulos de um triangulo
+
+let ang1 = 60, ang2 = 60 , ang3 = 60;
+
+if ((typeof ang1 !='number') || (typeof ang2 != 'number') || (typeof ang3 != 'number')){
+    console.log('ERRO, algum angulo e invalido!'); 
+}else {
+    resul = ang1 + ang2 + ang3;
+    if(resul == 180){
+        console.log("True")
+    }else{
+        console.log("False")
+    }
+
+}
+
