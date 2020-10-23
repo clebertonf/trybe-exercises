0cea251c4cd0190586431d435cc5f56c5c92746a
Cleberton Francisco <clebertonfgc@gmail.com>
2020-10-23 19:15:07 -0300

    Adicionando segundo exercicio sobre maior e menor numero

---
+++ exercises/bloco_4/dia_1/exercicio_02.js
@@ -0,0 +1,9 @@
+
+// Exercicio Qual maior numero
+let number1 = 5100, number2 = 258;
+
+if (number1 > number2){
+    console.log(number1, "e maior!");
+}else {
+    console.log(number2, "e maior");
+}
