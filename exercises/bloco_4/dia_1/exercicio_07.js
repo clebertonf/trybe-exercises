725ace687afc74477da29f4444e9db3b9a6e912d
Cleberton Francisco <clebertonfgc@gmail.com>
2020-10-24 20:54:16 -0300

    Criando exercicio 07 sobre conceitos de nota de 0 a 100 de A / F

---
+++ exercises/bloco_4/dia_1/exercicio_07.js
@@ -0,0 +1,20 @@
+let nota = -96;
+
+if (nota < 0 || nota > 100){
+    console.log("ERRO, nota invalida esta fora do intervalo, e menor que zero ou maior que 100!");
+}else{
+    if (nota >= 90){
+        console.log("Conceito A !");
+    }else if (nota >= 80)
+        console.log("Conceito B !");
+    else if(nota >= 70){
+        console.log("Conceito C !");
+    }else if(nota >= 60){
+        console.log("Conceito D !");
+    }else if (nota >= 50){
+        console.log("Conceito E !");
+    }else if (nota < 50){
+        console.log("Conceito F !");
+    }
+
+}
