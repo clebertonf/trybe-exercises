2b5c2f86a110f8245fc536261e05c5154b54c79f
Cleberton Francisco <clebertonfgc@gmail.com>
2020-10-24 21:57:41 -0300

    Exercicio 09 verificando se um dos numero e impar e imprime n atela true, caso contrario retorne falso

--- exercises/bloco_4/dia_1/exercicio_08.js
+++ exercises/bloco_4/dia_1/exercicio_08.js
@@ -1,6 +1,6 @@
-let Number1 = 11, Number2 = 11, Number3= 11;
+let number1 = 11, number2 = 11, number3= 11;
 
-if ((Number1 % 2 == 0) || (Number2 % 2 == 0) || (Number3 % 2 ==0)){ 
+if ((number1 % 2 == 0) || (number2 % 2 == 0) || (number3 % 2 ==0)){ 
     console.log(true);
 }else{
     console.log(false);
