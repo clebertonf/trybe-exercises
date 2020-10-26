38879b2338809e5ae7760fb8b6d50971c2c2b6bb
Cleberton Francisco <clebertonfgc@gmail.com>
2020-10-26 20:04:52 -0300

    Exercicio 08 criando um looping e inserindo valores dentro do array

---
+++ exercises/bloco_4/dia_2/exercicio_08.js
@@ -0,0 +1,11 @@
+/*
+Utilizando for, crie uma array que vá de 1 até 25 e imprima o resultado;
+*/
+
+let numbers = [];
+
+for (let valor =0; valor<=25; valor++){
+    numbers.push(valor);
+}
+
+console.log(numbers);
