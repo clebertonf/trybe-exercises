9790190039513db53ac2a331df6d9b30fb475480
Cleberton Francisco <clebertonfgc@gmail.com>
2020-10-29 11:16:11 -0300

    Refatorando exercicio de conceito de notas com funçoes

--- exercises/bloco_4/dia_1/exercicios_refatorados_com_funcoes/exercicio_07.js
+++ exercises/bloco_4/dia_1/exercicios_refatorados_com_funcoes/exercicio_07.js
@@ -1,20 +1,23 @@
-let nota = 95;
 
-if (nota < 0 || nota > 100){
-    console.log("ERRO, nota invalida esta fora do intervalo, e menor que zero ou maior que 100!");
-}else{
-    if (nota >= 90){
-        console.log("Conceito A !");
-    }else if (nota >= 80)
-        console.log("Conceito B !");
-    else if(nota >= 70){
-        console.log("Conceito C !");
-    }else if(nota >= 60){
-        console.log("Conceito D !");
-    }else if (nota >= 50){
-        console.log("Conceito E !");
-    }else if (nota < 50){
-        console.log("Conceito F !");
+conceito_nota(95);
+
+function conceito_nota(nota){
+    if (nota < 0 || nota > 100){
+        console.log("ERRO, nota invalida esta fora do intervalo, e menor que zero ou maior que 100!");
+    }else{
+        if (nota >= 90){
+            console.log("Conceito A !");
+        }else if (nota >= 80)
+            console.log("Conceito B !");
+        else if(nota >= 70){
+            console.log("Conceito C !");
+        }else if(nota >= 60){
+            console.log("Conceito D !");
+        }else if (nota >= 50){
+            console.log("Conceito E !");
+        }else if (nota < 50){
+            console.log("Conceito F !");
+        }
+    
     }
-
 }
