3c560d7fac51390cd81d0d0f2915748aeb94677e
Cleberton Francisco <clebertonfgc@gmail.com>
2020-10-29 11:10:01 -0300

    Refatorando com funçoes

--- exercises/bloco_4/dia_1/exercicios_refatorados_com_funcoes/exercicio_05.js
+++ exercises/bloco_4/dia_1/exercicios_refatorados_com_funcoes/exercicio_05.js
@@ -1,15 +1,17 @@
 // Exercicio 05 angulos de um triangulo
 
-let ang1 = 60 , ang2 = 60, ang3 = 60;
 
-if ((typeof ang1 !='number') || (typeof ang2 != 'number') || (typeof ang3 != 'number')){
-    console.log('ERRO, algum angulo e invalido!'); 
-}else {
-    resul = ang1 + ang2 + ang3;
-    if(resul == 180){
-        console.log("True")
-    }else{
-        console.log("False")
+angulo(60,60,60);
+
+function angulo(ang1,ang2,ang3){
+    if ((typeof ang1 !='number') || (typeof ang2 != 'number') || (typeof ang3 != 'number')){
+        console.log('ERRO, algum angulo e invalido!'); 
+    }else {
+        resul = ang1 + ang2 + ang3;
+        if(resul == 180){
+            console.log("True")
+        }else{
+            console.log("False")
+        }
     }
 }
-
