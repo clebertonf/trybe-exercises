3d83ead98d6d37f186bb95db5533c9dce80f8a16
Cleberton Francisco <clebertonfgc@gmail.com>
2020-10-29 11:27:33 -0300

    Refatorando exercicio de calculo de custo com funçoes

--- exercises/bloco_4/dia_1/exercicios_refatorados_com_funcoes/exercicio_10.js
+++ exercises/bloco_4/dia_1/exercicios_refatorados_com_funcoes/exercicio_10.js
@@ -1,13 +1,17 @@
 
-let custoProdutoInicial = 100;
+calcula_custo(50,100);
+
+function calcula_custo(custoProduto,Venda){
+let custoProdutoInicial = custoProduto;
 let imposto = custoProdutoInicial * 20 / 100;
 let custoFinal = custoProdutoInicial + imposto;
-let valorVenda = 210;
-let quantidadeVenda = 1000;
+let valorVenda = Venda;
+const quantidadeVenda = 1000;
 
 if (custoProdutoInicial <= 0 || valorVenda  <= 0 || quantidadeVenda <= 0){
     console.log("ERRO, nehum destes valores podem ser  zero ou inferiores!")
 }else{
     lucro = (valorVenda - custoFinal) * quantidadeVenda;
     console.log(lucro);
+}
 }
