36d8022e956989b7f0ef462732d98af3852c7ea7
Cleberton Francisco <clebertonfgc@gmail.com>
2020-10-29 11:07:42 -0300

    Refatorando com funçoes

--- exercises/bloco_4/dia_1/exercicios_refatorados_com_funcoes/exercicio_04.js
+++ exercises/bloco_4/dia_1/exercicios_refatorados_com_funcoes/exercicio_04.js
@@ -1,14 +1,16 @@
 
 //Exercicio 04 numero positivo ou negativo ou zero
 
-let number = 15;
+numero(0);
 
-if (number > 0){
-    console.log("Positive")
-}else if (number < 0){
-    console.log("Negative")
-}else if (number === 0){
-    console.log("Zero")
-}else{
-    console.log("Nehum valor definido");
+function numero(number){
+    if (number > 0){
+        console.log("Positive")
+    }else if (number < 0){
+        console.log("Negative")
+    }else if (number === 0){
+        console.log("Zero")
+    }else{
+        console.log("Nehum valor definido");
+    }
 }
