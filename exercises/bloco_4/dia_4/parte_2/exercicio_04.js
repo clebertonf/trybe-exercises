f2eccf555238bff1f09503f8ac9eb491356bd144
Cleberton Francisco <clebertonfgc@gmail.com>
2020-11-01 16:15:46 -0300

    Refatorando exercicio 04

--- exercises/bloco_4/dia_4/parte_2/exercicio_04.js
+++ exercises/bloco_4/dia_4/parte_2/exercicio_04.js
@@ -4,19 +4,24 @@ Array de teste: ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];.
 Valor esperado no retorno da função: Fernanda.
 */
 
-let names = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
-let contagem = 0;
-let maior = 0;
+let listNames = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
 
-for (let x in names){
-    contagem = names[x].length;
-    
-    if (contagem > maior){
-        maior = contagem;   
-    } 
+function maiorArray(names) {
+    let contagem = 0;
+    let maior = 0;
+
+    for (let x in names) {
+        contagem = names[x].length;
+
+        if (contagem > maior) {
+            maior = contagem;
+        }
+    }
+
+    for (let y in names) {
+        if (names[y].length === maior)
+            console.log(names[y])
+    }
 }
 
-for (let y in names){
-    if (names[y].length === maior)
-    console.log(names[y])
-}
+maiorArray(listNames);
