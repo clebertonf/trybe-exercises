6cbd46038ac6a2c5e8609a830db506d46f9507c8
Cleberton Francisco <clebertonfgc@gmail.com>
2020-11-01 15:06:28 -0300

    Exercicio 02 posiçao do amior valor

---
+++ exercises/bloco_4/dia_4/parte_2/exercicio_02.js
@@ -0,0 +1,20 @@
+/*
+Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+Array de teste: [2, 3, 6, 7, 10, 1];.
+Valor esperado no retorno da função: 4.
+
+*/
+
+let array = [2, 3, 6, 16, 10, 1];
+
+let maior = 0;
+let posicao =0;
+for (let key in array) {
+    if (array[key] > maior) {
+        maior = array[key];
+        posicao = key , array[key];
+    }
+}
+console.log('Maior ' + maior);
+console.log('Posicao ' + posicao);
+
