050af4fc93fcf27e1213b26233ecea43d124ddfb
Cleberton Francisco <clebertonfgc@gmail.com>
2020-11-01 16:12:25 -0300

    Refatorando exercicio 02

--- exercises/bloco_4/dia_4/parte_2/exercicio_02.js
+++ exercises/bloco_4/dia_4/parte_2/exercicio_02.js
@@ -5,16 +5,21 @@ Valor esperado no retorno da função: 4.
 
 */
 
-let array = [2, 3, 6, 16, 10, 1];
 
-let maior = 0;
-let indice =0;
-for (let key in array) {
-    if (array[key] > maior) {
-        maior = array[key];
-        indice = key;
+let array = [2, 3, 6, 16, 100, 1];
+
+function posicaoMaior(array) {
+    let maior = 0;
+    let indice = 0;
+    for (let key in array) {
+        if (array[key] > maior) {
+            maior = array[key];
+            indice = key;
+        }
     }
+    console.log('Maior ' + maior);
+    console.log('Posicao ' + indice);
 }
-console.log('Maior ' + maior);
-console.log('Posicao ' + indice);
 
+
+posicaoMaior(array);
