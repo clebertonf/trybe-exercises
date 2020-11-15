5c1500a95cec85627d838ac844100ca67a5fb8a9
Cleberton Francisco <clebertonfgc@gmail.com>
2020-11-15 13:56:35 -0300

    Primeiros requisitos do exercicio

---
+++ exercises/bloco_6/dia_1/script.js
@@ -0,0 +1,42 @@
+let estados = [
+    'AC - Acre',
+    'AL - Alagoas',
+    'AP - Amapá',
+    'AM - Amazonas',
+    'BA - Bahia',
+    'CE - Ceará',
+    'DF - Distrito Federal',
+    'ES - Espírito Santo',
+    'GO - Goías',
+    'MA - Maranhão',
+    'MT - Mato Grosso',
+    'MS - Mato Grosso do Sul',
+    'MG - Minas Gerais',
+    'PA - Pará',
+    'PB - Paraíba',
+    'PR - Paraná',
+    'PE - Pernambuco',
+    'PI - Piauí',
+    'RJ - Rio de Janeiro',
+    'RN - Rio Grande do Norte',
+    'RS - Rio Grande do Sul',
+    'RO - Rondônia',
+    'RR - Roraíma',
+    'SC - Santa Catarina',
+    'SP - São Paulo',
+    'SE - Sergipe',
+    'TO - Tocantins'
+];
+
+function comobobox(states) {
+    let inputEstado = document.querySelector('#input-estado');
+
+    for (let index = 0; index < states.length; index += 1) {
+        let option = document.createElement('option');
+        option.innerText = states[index];
+        option.value = states[index];
+        inputEstado.appendChild(option);
+    }
+}
+comobobox(estados)
+
