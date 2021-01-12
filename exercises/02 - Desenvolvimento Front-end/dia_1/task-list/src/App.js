14a9ad96cecf7163ad81e07c4fa77f545da6a82f
Cleberton <clebertonfgc@gmail.com>
2021-01-11 21:20:10 -0300

    Exercicio 01

---
+++ exercises/02 - Desenvolvimento Front-end/dia_1/task-list/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  const tasks = ['Tarefa 1', 'Tarefa 2', 'Tarefa 3', 'Tarefa 4', 'Tarefa 5', 'Tarefa 6'];
+
+  const task = (value) => {
+    return (
+      value.map(data => <li>{data}</li> )
+    );
+  }
+  return ( <div>
+    {task(tasks)}
+  </div>
+  )
+  }
+
+export default App;
